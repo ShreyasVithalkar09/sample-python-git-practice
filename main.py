4298832b5bef849dd629d721f6a323d482f30f5c
ShreyasVithalkar09 <shreyas.vithalkar08@gmail.com>
2024-05-12 01:19:18 +0530

    Moved function to greetings file

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def print_hi(message, name):
-    print(f'Hi this is, {message}')
-    print(f"Welcome to Pycharm, {name}")
-    print("This line of code is from develop branch...")
-    print("This line is from main..")
-
+import greetings
 
 if __name__ == '__main__':
-    print_hi('Simple Python project', 'Harry')
+    greetings.greet('Simple Python project', 'Harry')
