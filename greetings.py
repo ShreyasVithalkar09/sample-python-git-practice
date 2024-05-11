cd2f921be3da557ab801c71e1f1add54611246ff
ShreyasVithalkar09 <shreyas.vithalkar08@gmail.com>
2024-05-12 01:17:58 +0530

    Created new file greetings

---
+++ greetings.py
@@ -0,0 +1,5 @@
+def greet(message, name):
+    print(f'Hi this is, {message}')
+    print(f"Welcome to Pycharm, {name}")
+    print("This line of code is from develop branch...")
+    print("This line is from main..")
