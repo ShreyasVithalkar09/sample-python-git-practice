68e6698fcbe843fd38cfed679828c1c097bc6963
ShreyasVithalkar09 <shreyas.vithalkar08@gmail.com>
2024-05-11 12:56:00 +0530

    Formatted code

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(message, name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi this is, {message}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi this is, {message}')
     print(f"Welcome to Pycharm, {name}")
     print("This line of code is from develop branch...")
     print("This line is from main..")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Simple Python project', 'Harry')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
